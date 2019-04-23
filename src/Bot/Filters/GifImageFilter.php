87cf68ae14347084215254316deffa5fe437e8fc
romach <romach3@gmail.com>
2019-04-24 04:16:22 +0500

    new post type

--- src/Bot/Filters/GifImageFilter.php
+++ src/Bot/Filters/GifImageFilter.php
@@ -9,8 +9,11 @@ class GifImageFilter extends AbstractFilter
 
     public function transform(array $message, array $item): array
     {
-        if (mb_substr($message['url'], -4) === '.gif') {
+        if (mb_substr($message['url'], -4) === '.gif' || mb_substr($message['url'], -5) === '.gifv') {
             $message['hint'] = 'gif:video';
+        }
+        if (mb_substr($message['url'], -5) === '.gifv') {
+            $message['url'] = mb_substr($message['url'], 0, mb_strlen($message['url']) - 1);
         }
 
         return ['message' => $message, 'item' => $item];
--- src/Bot/Filters/PrimaryLinkFilter.php
+++ src/Bot/Filters/PrimaryLinkFilter.php
@@ -9,14 +9,19 @@ class PrimaryLinkFilter extends AbstractFilter
 
     public function transform(array $message, array $item): array
     {
-        if (isset($item['data']['crosspost_parent_list'][0]) && $item['data']['crosspost_parent_list'][0]['post_hint'] !== 'link') {
-            $message['thumbnail'] = $item['data']['crosspost_parent_list'][0]['thumbnail'];
-            $message['text'] = $item['data']['crosspost_parent_list'][0]['selftext'];
-            $message['hint'] = $item['data']['crosspost_parent_list'][0]['post_hint'] ?? 'self';
-            $message['url'] = $item['data']['crosspost_parent_list'][0]['url'];
-            $item['data'] = $item['data']['crosspost_parent_list'][0];
+        if (isset($item['data']['crosspost_parent_list'][0])) {
+           if (!isset($item['data']['crosspost_parent_list'][0]['post_hint'])) {
+               if (mb_substr($message['url'], -4) === '.gif' || mb_substr($message['url'], -5) === '.gifv') {
+                   $message['hint'] = 'image';
+               }
+           } else if ($item['data']['crosspost_parent_list'][0]['post_hint'] !== 'link') {
+               $message['thumbnail'] = $item['data']['crosspost_parent_list'][0]['thumbnail'];
+               $message['text'] = $item['data']['crosspost_parent_list'][0]['selftext'];
+               $message['hint'] = $item['data']['crosspost_parent_list'][0]['post_hint'] ?? 'self';
+               $message['url'] = $item['data']['crosspost_parent_list'][0]['url'];
+               $item['data'] = $item['data']['crosspost_parent_list'][0];
+           }
         }
-
         return ['message' => $message, 'item' => $item];
     }
 
