aaac4cb9088c34518dd75346795dcd092cccbf1d
romach <romach3@gmail.com>
2019-04-24 03:41:19 +0500

    test: two post pages

--- src/Bot/Commands/GetPostsFromReddit.php
+++ src/Bot/Commands/GetPostsFromReddit.php
@@ -8,7 +8,13 @@ class GetPostsFromReddit
     public function handle(): void
     {
         $response = json_decode(file_get_contents('https://www.reddit.com/r/Pikabu/hot/.json?limit=100'), true);
-        file_put_contents('./storage/posts.json', json_encode($response['data']['children'], JSON_UNESCAPED_UNICODE));
+        $posts = $response['data']['children'];
+        $after = $response['data']['after'];
+        for ($i = 0; $i < 1; $i++) {
+            $response = json_decode(file_get_contents("https://www.reddit.com/r/Pikabu/hot/.json?limit=100&after={$after}"), true);
+            $posts = array_merge($posts, $response['data']['children']);
+        }
+        file_put_contents('./storage/posts.json', json_encode($posts, JSON_UNESCAPED_UNICODE));
     }
 
 }
