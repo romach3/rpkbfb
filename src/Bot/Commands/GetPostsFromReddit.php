a13ac454365bfb9b97bd2d667dddfa01ab3b612b
romach <romach3@gmail.com>
2019-11-11 18:18:21 +0500

    add user agent

--- src/Bot/Commands/GetPostsFromReddit.php
+++ src/Bot/Commands/GetPostsFromReddit.php
@@ -14,12 +14,18 @@ class GetPostsFromReddit
     public function handle(): void
     {
         $posts = [];
+        $context = stream_context_create([
+            "http" => [
+                "method" => "GET",
+                "header" => "User-Agent: rpikabufeed_bot\r\n"
+            ]
+        ]);
         foreach ($this->config['subreddits'] as $uri) {
-            $response = json_decode(file_get_contents($uri.'.json?limit=100'), true);
+            $response = json_decode(file_get_contents($uri.'.json?limit=100', false, $context), true);
             $posts = array_merge($posts, $response['data']['children']);
             $after = $response['data']['after'];
             for ($i = 2; $i <= $this->config['pages']; $i++) {
-                $response = json_decode(file_get_contents($uri.".json?limit=100&after={$after}"), true);
+                $response = json_decode(file_get_contents($uri.".json?limit=100&after={$after}", false, $context), true);
                 $posts = array_merge($posts, $response['data']['children']);
             }
         }
