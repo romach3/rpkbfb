5440ccfcf5916497c97d9c4b1f0e419e94e28152
roman <romanch3@gmail.com>
2022-04-23 15:10:43 +0500

    example config

--- config.example.php
+++ config.example.php
@@ -1,18 +1,38 @@
 <?php
+use App\Bot\Filters\GifImageFilter;
+use App\Bot\Filters\HostedVideoFilter;
+use App\Bot\Filters\LongTextFilter;
+use App\Bot\Filters\MFilter;
+use App\Bot\Filters\NSFWThumbnailFilter;
+use App\Bot\Filters\PrimaryLinkFilter;
+use App\Bot\Filters\RichVideoFilter;
 
 return [
+    'tg' => [
+        'proxy' => env('GUZZLE_CLIENT_PROXY'),
+        'token' => env('BOT_TOKEN'),
+    ],
+    'filters' => [
+        PrimaryLinkFilter::class,
+        LongTextFilter::class,
+        NSFWThumbnailFilter::class,
+        GifImageFilter::class,
+        HostedVideoFilter::class,
+        RichVideoFilter::class,
+        MFilter::class
+    ],
     'channels' => [
+        [
+            'name' => '@rpikabufeed',
+            'nsfw' => false,
+        ],
         [
             'name' => '@rpikabufeed_nsfw',
             'nsfw' => true,
         ],
-        [
-            'name' => '@rpikabufeed',
-            'nsfw' => false,
-        ]
     ],
     'subreddits' => [
-        'https://www.reddit.com/r/Pikabu/hot/'
+        'https://www.reddit.com/r/Pikabu/hot/',
     ],
     'sendedListSize' => 500,
     'disableNotification' => true,
